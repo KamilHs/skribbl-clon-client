442e7338df838121a3127046e6ccc24766e7173d
KamilHs <kamilsalimli1940@gmail.com>
2021-03-21 16:18:37 +0400

    refactor: socket events logic updated

--- src/middlewares/socketMiddleware.ts
+++ src/middlewares/socketMiddleware.ts
@@ -1,6 +1,6 @@
 import { AnyAction, Middleware, MiddlewareAPI, Dispatch } from "redux";
 import { push } from "connected-react-router";
-import socketIOClient, { Socket } from "socket.io-client";
+import socketIOClient from "socket.io-client";
 
 import { homeActions, lobbyActions } from "../redux/actions";
 import { RootState } from "../redux/store";
@@ -14,7 +14,6 @@ import { isHomeError, GET_PLAYERS_DATA, IHomeResponse } from "../socket/types";
 import { LOBBY_ROUTES } from "../modules/Lobby";
 
 const handleCreateAndJoinRoom = (
-    socket: typeof Socket,
     storeApi: MiddlewareAPI<Dispatch<AnyAction>, RootState>,
     res: IHomeResponse
 ) => {
@@ -23,9 +22,6 @@ const handleCreateAndJoinRoom = (
     } else {
         storeApi.dispatch(lobbyActions.setRoomId(res.roomId));
         storeApi.dispatch(push(`${LOBBY_ROUTES.MAIN}/${res.roomId}`));
-        socket.on(GET_PLAYERS_DATA, (data: IPlayerData[] | null) =>
-            storeApi.dispatch(lobbyActions.setPlayerData(data))
-        );
     }
 };
 
@@ -37,18 +33,22 @@ export const socketMiddleware = (url: string) => {
     });
 
     const middleware: Middleware<{}, RootState> = (storeApi) => {
+        socket.on(GET_PLAYERS_DATA, (data: IPlayerData[] | null) =>
+            storeApi.dispatch(lobbyActions.setPlayerData(data))
+        );
+
         return (next) => (action: AllActionTypes) => {
             switch (action.type) {
                 case CREATE_ROOM:
                     socket.emit(CREATE_ROOM, { nickname: action.payload });
                     socket.on(CREATE_ROOM, (res: IHomeResponse) =>
-                        handleCreateAndJoinRoom(socket, storeApi, res)
+                        handleCreateAndJoinRoom(storeApi, res)
                     );
                     break;
                 case JOIN_ROOM:
                     socket.emit(JOIN_ROOM, action.payload);
                     socket.on(JOIN_ROOM, (res: IHomeResponse) =>
-                        handleCreateAndJoinRoom(socket, storeApi, res)
+                        handleCreateAndJoinRoom(storeApi, res)
                     );
                     break;
                 default:
